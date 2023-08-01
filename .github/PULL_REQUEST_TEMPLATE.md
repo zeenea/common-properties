28a20895c8975865b7d326c092e3337f23ba9cea
AlexgCH <acohen@zeenea.com>
2023-08-01 09:41:30 +0200

    [ZEE-4578] common-properties lib version 0.1

---
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,7 @@
+This PR does the following:
+- TODO
+
+QA
+- TODO
+
+See https://zeenea.atlassian.net/browse/ZEE-${id}
---
+++ lib/build.gradle.kts
@@ -0,0 +1,67 @@
+plugins {
+    // Apply the java-library plugin for API and implementation separation.
+    `java-library`
+    `maven-publish`
+    id("com.diffplug.spotless") version "6.20.0"
+}
+
+group = "zeenea"
+version = rootProject.file("version").readLines()[0]
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+tasks.withType<JavaCompile> {
+    options.compilerArgs.add("--enable-preview")
+}
+tasks.test {
+    jvmArgs("--enable-preview")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // Use JUnit test framework.
+    testImplementation("junit:junit:4.13.2")
+
+    // This dependency is exported to consumers, that is to say found on their compile classpath.
+    api("org.apache.commons:commons-math3:3.6.1")
+
+    // This dependency is used internally, and not exposed to consumers on their own compile classpath.
+    implementation("com.google.guava:guava:30.1.1-jre")
+}
+
+tasks.jar {
+    archiveBaseName.set("common-properties")
+}
+
+spotless {
+    java {
+        googleJavaFormat()
+        toggleOffOn()
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            url = uri("https://maven.pkg.github.com/zeenea/common-properties")
+            credentials {
+                username =
+                        System.getenv("GITHUB_ACTOR") ?: project.findProperty("github.actor") as String?
+                password =
+                        System.getenv("GITHUB_TOKEN") ?: project.findProperty("github.token") as String?
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            artifactId = "common-properties"
+            from(components["java"])
+        }
+    }
+}
