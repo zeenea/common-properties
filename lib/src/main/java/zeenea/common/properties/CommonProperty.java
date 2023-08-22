db23266b5f4c6ead6a0a43845372bd9bc70b8115
Corentin Bigot <cbigot@zeenea.com>
2023-08-22 17:08:05 +0200

    :fire: [ZEE-4504] delete code and related uuids

--- lib/src/main/java/zeenea/common/properties/CommonProperty.java
+++ lib/src/main/java/zeenea/common/properties/CommonProperty.java
@@ -2,40 +2,26 @@ package zeenea.common.properties;
 
 import java.util.Objects;
 import java.util.Optional;
-import java.util.Set;
 import java.util.UUID;
 import javax.annotation.Nullable;
 import zeenea.common.properties.type.Type;
 
 public final class CommonProperty {
   private final UUID uuid;
-  private final String code;
   private final String defaultName;
   @Nullable private final String defaultDescription;
   private final Type type;
-  private final Set<UUID> relatedUuids;
 
   public CommonProperty(
-      UUID uuid,
-      String code,
-      String defaultName,
-      @Nullable String defaultDescription,
-      Type type,
-      Set<UUID> relatedUuids) {
+      UUID uuid, String defaultName, @Nullable String defaultDescription, Type type) {
     this.uuid = uuid;
-    this.code = code;
     this.defaultName = defaultName;
     this.defaultDescription = defaultDescription;
     this.type = type;
-    this.relatedUuids = Set.copyOf(relatedUuids);
   }
 
   public UUID uuid() {
     return uuid;
-  }
-
-  public String code() {
-    return code;
   }
 
   public String defaultName() {
@@ -55,26 +41,20 @@ public final class CommonProperty {
     return type;
   }
 
-  public Set<UUID> relatedUuids() {
-    return relatedUuids;
-  }
-
   @Override
   public boolean equals(Object obj) {
     if (obj == this) return true;
     if (obj == null || obj.getClass() != this.getClass()) return false;
     var that = (CommonProperty) obj;
     return Objects.equals(this.uuid, that.uuid)
-        && Objects.equals(this.code, that.code)
         && Objects.equals(this.defaultName, that.defaultName)
         && Objects.equals(this.defaultDescription, that.defaultDescription)
-        && Objects.equals(this.type, that.type)
-        && Objects.equals(this.relatedUuids, that.relatedUuids);
+        && Objects.equals(this.type, that.type);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(uuid, code, defaultName, defaultDescription, type, relatedUuids);
+    return Objects.hash(uuid, defaultName, defaultDescription, type);
   }
 
   @Override
@@ -82,9 +62,6 @@ public final class CommonProperty {
     return "CommonProperty["
         + "uuid="
         + uuid
-        + ", "
-        + "code="
-        + code
         + ", "
         + "defaultName="
         + defaultName
@@ -94,9 +71,6 @@ public final class CommonProperty {
         + ", "
         + "type="
         + type
-        + ", "
-        + "relatedUuids="
-        + relatedUuids
         + ']';
   }
 }
