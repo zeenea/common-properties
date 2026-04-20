746c159d87d83259ee0dbd5a383c38c57e13f4e3
lionelv-actian <lionel.vigier@hcl-software.com>
2026-04-20 17:33:00 +0200

    [ZEE-10000] removed unnecessary java dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,2 @@
 rootProject.name = "common-properties"
 include("lib")
-
-// Allow composite builds to substitute "zeenea:common-properties" with the :lib subproject
-project(":lib").name = "common-properties"
